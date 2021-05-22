9d048e1702761a2a762b6438966cde1e99d6dd3d
Lee Hyeri <hyeri501@naver.com>
2021-05-22 19:28:08 +0900

    15,16,17,18 TextView 위치 protected void onCreate 내부로 옮김

--- app/src/main/java/com/example/myapplication/PatientstateA.java
+++ app/src/main/java/com/example/myapplication/PatientstateA.java
@@ -7,14 +7,14 @@ import android.widget.TextView;
 
 public class PatientstateA extends AppCompatActivity {
 
-    TextView heart_value = (TextView)findViewById(R.id.heart_value);
-    TextView heart_state = (TextView)findViewById(R.id.heart_state);
-    TextView oxygen_value = (TextView)findViewById(R.id.oxygen_value);
-    TextView oxygen_state = (TextView)findViewById(R.id.oxygen_state);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patientstate_a);
+
+        TextView heart_value = (TextView)findViewById(R.id.heart_value);
+        TextView heart_state = (TextView)findViewById(R.id.heart_state);
+        TextView oxygen_value = (TextView)findViewById(R.id.oxygen_value);
+        TextView oxygen_state = (TextView)findViewById(R.id.oxygen_state);
     }
 }
